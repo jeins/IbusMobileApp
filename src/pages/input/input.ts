5338466dbf364b530d8b72c39841e790fa847319
Muhammad Juan Akbar <mail@mjuan.me>
2017-02-21 00:02:12 +0100

    finishing design input page

--- src/pages/input/input.ts
+++ src/pages/input/input.ts
@@ -1,22 +1,27 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
 
 /*
-  Generated class for the Input page.
+ Generated class for the Input page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+ See http://ionicframework.com/docs/v2/components/#navigation for more info on
+ Ionic pages and navigation.
+ */
 @Component({
-  selector: 'page-input',
-  templateUrl: 'input.html'
+    selector: 'page-input',
+    templateUrl: 'input.html'
 })
 export class InputPage {
+    isAddBillImage: boolean;
+    product: Object;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+        this.isAddBillImage = false;
+        this.product = {name: '', description: '', purchasePrice: {currency: 'euro'}, sellingPrice: {currency: 'rupiah'}}
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad InputPage');
-  }
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad InputPage');
+    }
 
 }
